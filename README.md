521f9e39903b9293178088d06877204712d7639c
forgreed <gugreed@gmail.com>
2014-06-05 17:28:57 -0400

    First Commit!

---
+++ README.md
@@ -0,0 +1,9 @@
+# SUPER SIMPLE File Uploader
+
+### ExpressJS server using fs and busboy to upload multipart data
+
+### Gettin' it going:
+	1. `npm install`
+	2. `node app`
+
+### That's it...
---
+++ app.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var busboy = require('connect-busboy');
+app.use(busboy());
+
+app.listen(3000);
+console.log('Server listening on port 8080');
+
+app.get('/', function(request, response) {
+	fs.readFile('upload.html',function (err, data) {
+		if (err) { console.log(err); }
+	    response.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+	    response.write(data);
+    	response.end();
+	});
+});
+
+app.post('/upload',function(request,response) {
+	var fstream;
+    request.pipe(request.busboy);
+    request.busboy.on('file', function(fieldname, file, filename) {
+    	console.log('Uploading: ' + filename);
+    	fstream = fs.createWriteStream('./tehFiles/' + filename);
+    	file.pipe(fstream);
+    	fstream.on('close', function() {
+    		response.redirect('back');
+    	});
+    });
+});
