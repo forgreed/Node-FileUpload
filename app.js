ee8437c71a04d5404e8f80fe860e4ac120875501
forgreed <gugreed@gmail.com>
2014-08-06 20:06:11 -0400

    Rename to be serious

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
 /*  
-*    Super Easy File Uploader
-*   @author greed@varrow.com
-*   June 2014
-*   Use this to begin developing an understanding of the extraordinary powers of Node.js and ExpressJS!
-*/ 
+ *   Super Easy File Uploader
+ *   @author greed@varrow.com
+ *   June 2014
+ *   Use this to begin developing an understanding of the extraordinary powers of Node.js and ExpressJS!
+ */
 var fs = require('fs');
 var express = require('express');
 var app = express();
 var busboy = require('connect-busboy');
 app.use(busboy());
 
-var port = 3000;
-app.listen(port);
+var port = (process.env.VCAP_APP_PORT || 3000);
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+app.listen(port, host);
 console.log('Server listening on port ' + port);
 
 app.get('/', function(request, response) {
-	fs.readFile('upload.html', function (error, data) {
-		if(error) { 
-            console.log(error); 
-        }
-	    response.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': data.length});
-	    response.write(data);
-    	response.end();
-	});
+  fs.readFile('upload.html', function(error, data) {
+    if (error) {
+      console.log(error);
+    }
+    response.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Content-Length': data.length
+    });
+    response.write(data);
+    response.end();
+  });
 });
 
-app.post('/upload',function(request, response) {
-	var fstream;
-    request.pipe(request.busboy);
-    request.busboy.on('file', function(fieldname, file, filename) {
-    	console.log('Uploading: ' + filename);
-    	fstream = fs.createWriteStream('./tehFiles/' + filename);
-    	file.pipe(fstream);
-    	fstream.on('close', function() {
-    		response.redirect('back');
-            console.log('Uploaded to ' + fstream.path);
-    	});
+app.post('/upload', function(request, response) {
+  var fstream;
+  request.pipe(request.busboy);
+  request.busboy.on('file', function(fieldname, file, filename) {
+    console.log('Uploading: ' + filename);
+    fstream = fs.createWriteStream('./tmp/' + filename);
+    file.pipe(fstream);
+    fstream.on('close', function() {
+      response.redirect('back');
+      console.log('Uploaded to ' + fstream.path);
     });
+  });
 });
